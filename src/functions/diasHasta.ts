9aaa9054842ad79932e992c72ad51efcdc6cba1c
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2023-08-12 16:04:02 +0200

    Corrigiendo diasHasta para mostrar correctamente los días restantes redondeando el valor hacía arriba

--- src/functions/diasHasta.ts
+++ src/functions/diasHasta.ts
@@ -2,7 +2,7 @@
 function diasHasta(fecha:Date):any {
     let hoy = new Date();
     //@ts-ignore
-    return ((fecha - hoy) / (1000 * 60 * 60 * 24)).toFixed(0);
+    return Math.ceil((fecha - hoy) / (1000 * 60 * 60 * 24));
 }
 
 module.exports = {

e55164805ceafab0581077c22a48378157841d43
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2023-11-28 21:21:25 -0300

    Acciones

--- src/commands/anualizarinflacion.ts
+++ src/commands/anualizarinflacion.ts
@@ -14,7 +14,7 @@ module.exports = {
       let anualizado: number = (((interaction.options.getNumber('mensual') / 100) + 1) ** 12 - 1) * 100;
       const embed:Discord.EmbedBuilder = new Discord.EmbedBuilder()
           .setTitle("Inflación mensual anualizada")
-          .setDescription("La economía tiene un atraso de 18 a 24 meses.")
+          .setDescription("La economía tiene un resago de 18 a 24 meses.")
           .setColor("#f82f40")
           .setThumbnail("https://cdn.discordapp.com/attachments/802944543510495292/1176940661476438077/inflation.png?ex=6570b268&is=655e3d68&hm=8158c1d52bff25b6a78cf72b991a27c96a599b95bb3cd229012c31379da38e02&")
           .addFields(

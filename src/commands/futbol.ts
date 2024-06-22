a084441c09da53ddf590605603274f2a840e3c3a
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-06-22 16:56:21 -0300

    Fulvo

--- src/commands/futbol.ts
+++ src/commands/futbol.ts
@@ -6,45 +6,61 @@ module.exports = {
     .setName("futbol")
     .setDescription("Muestra cuántos días faltan para  los siguientes partidos de la selección"),
 
-    async run(client, interaction){   
+    async run(client, interaction){
+      
+      const proximosPartidos = [
+        {
+          fecha: '2024-06-25',
+          rival: ':flag_cl:'
+        },
+        {
+          fecha: '2024-06-29',
+          rival: ':flag_pe:'
+        },
+        {
+        fecha: "2024-09-05",
+        rival: ":flag_cl:"
+      },
+      {
+        fecha: "2024-09-10",
+        rival: ":flag_co:"
+      },
+      {
+        fecha: "2024-10-10",
+        rival: ":flag_vz:"
+      },
+      {
+        fecha: "2024-10-15",
+        rival: ":flag_bo:"
+      },
+      {
+        fecha: "2024-11-14",
+        rival: ":flag_py:"
+      },
+      {
+        fecha: "2024-11-19",
+        rival: ":flag_pe:"
+      }
+
+    ]
+
+      const fields = proximosPartidos.map(partido => {
+        return {
+          name: `:flag_ar: vs ${partido.rival} \n(${partido.fecha})`,
+          value: `Faltan ${diasHasta(new Date(partido.fecha))} días`,
+          inline: true
+        }
+      })
+
+      console.log(fields)
       const embed:Discord.EmbedBuilder = new Discord.EmbedBuilder()
           .setTitle("Tiempo hasta los siguientes partidos de la selección Argentina")
           .setColor("#7eb2fa")
           .setDescription("Tiempo hasta los siguientes partidos de la selección Argentina")
           .setThumbnail("https://cdn.discordapp.com/attachments/802944543510495292/929121012275093524/camiseta-de-futbol.png")
           .addFields(
-            { 
-              name: ":flag_ar: vs :flag_cl: \n(05/09/2024)", 
-              value: "Faltan " + diasHasta(new Date("2024-09-05")) + " días",  inline: true 
-            },
-            { 
-              name: ":flag_ar: vs :flag_co: \n(10/09/2024)", 
-              value: "Faltan " + diasHasta(new Date("2024-09-10")) + " días",  inline: true 
-            },
-            { 
-              name: ":flag_ar: vs :flag_ve: \n(10/10/2024)", 
-              value: "Faltan " + diasHasta(new Date("2024-10-10")) + " días",  inline: true 
-            },
-            { 
-              name: ":flag_ar: vs :flag_bo: \n(15/10/2024)", 
-              value: "Faltan " + diasHasta(new Date("2024-10-15")) + " días",  inline: true 
-            },
-            { 
-              name: ":flag_ar: vs :flag_py: \n(14/11/2024)", 
-              value: "Faltan " + diasHasta(new Date("2024-11-14")) + " días",  inline: true 
-            },
-            { 
-              name: ":flag_ar: vs :flag_pe: \n(19/11/2024)", 
-              value: "Faltan " + diasHasta(new Date("2024-11-24")) + " días",  inline: true 
-            },
-            { 
-              name: ":flag_ar: vs :flag_uy: \n(20/03/2025)", 
-              value: "Faltan " + diasHasta(new Date("2025-03-20")) + " días",  inline: true 
-            },
-            { 
-              name: ":flag_ar: vs :flag_br: \n(25/03/2025)", 
-              value: "Faltan " + diasHasta(new Date("2025-03-25")) + " días",  inline: true 
-            })         
+            fields
+          ) 
           return interaction.reply({ embeds: [embed] });
 
     }

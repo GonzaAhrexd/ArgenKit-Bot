2b8beb9cd7a10ef4f37dbaecf559ed374a69f43f
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2024-10-22 09:48:03 -0300

    Porcentaje

--- src/commands/datos.ts
+++ src/commands/datos.ts
@@ -50,22 +50,20 @@ module.exports = {
           // axios.get('https://api.argentinadatos.com/v1/finanzas/indices/riesgo-pais/ultimo'),
           axios.get('https://api.argentinadatos.com/v1/finanzas/indices/riesgo-pais/')
         ])
-
+        // Toma el último dato del array
         const ultimoDato = riesgo.data[riesgo.data.length - 1]
+        // Toma el penúltimo dato del array
         const datoAnterior = riesgo.data[riesgo.data.length - 2]
-
-
-        console.log(ultimoDato)
-        console.log(datoAnterior)
-
-        let isCambioRiesgo = ultimoDato['valor'] > datoAnterior['valor'] ? "🔺" : ultimoDato['valor'] == datoAnterior['valor'] ? "⏸️" : "<:flechashaciaabajo:1210747546096369664>"
-
+        // Calcula el porcentaje de cambio entre el último dato y el penúltimo
+        let porcentajeCambio = (((ultimoDato['valor'] - datoAnterior['valor']) / datoAnterior['valor']) * 100).toFixed(2)  
+        // Calcula si el riesgo país subió, bajó o se mantuvo igual
+        let isCambioRiesgo =  ultimoDato['valor'] > datoAnterior['valor'] ? "🔺" : ultimoDato['valor'] == datoAnterior['valor'] ? "⏸️" : "<:flechashaciaabajo:1210747546096369664>"
         const embed: Discord.EmbedBuilder = new Discord.EmbedBuilder()
           .setTitle("Riesgo País")
           .setColor("#e6306c")
           .setDescription("El riesgo país es todo riesgo inherente a las inversiones y a las financiaciones en un país en contraste con otro.")
           .setThumbnail("https://cdn.discordapp.com/attachments/802944543510495292/1177075689195835422/benchmarking.png?ex=65713029&is=655ebb29&hm=eb99e3c29ae5f5c67de55ede357d6e7501752bb2a5a08f577f4e4395fa6259ee&")
-          .addFields({ name: "Valor :chart_with_upwards_trend: ", value: `${ultimoDato['valor']} puntos ${isCambioRiesgo}` })
+          .addFields({ name: "Valor :chart_with_upwards_trend: ", value: `${ultimoDato['valor']} puntos ${isCambioRiesgo} ${porcentajeCambio}%` })
         await wait(3000)
         await interaction.editReply({ embeds: [embed] });
       } catch (error) {

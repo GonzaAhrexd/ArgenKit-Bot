fd8d99dbcbd8b4368ef85723f72693e6fab00c96
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2025-02-06 09:19:47 -0300

    Netflix precios

--- src/commands/servicios.ts
+++ src/commands/servicios.ts
@@ -78,10 +78,10 @@ module.exports = {
         .setColor('#9a0611')
         .setThumbnail("https://cdn.discordapp.com/attachments/802944543510495292/1180334885953609810/netflix.png?ex=657d0b86&is=656a9686&hm=711e7fc4fb8376b9efed01f54bc53c131331ae4d611e773840537a3f0d8925d3&")
         .addFields(
-          { name: "Básico:", value: "ARS" + formatoPrecio(total51(4299), "ARS"), inline: true },
-          { name: "Estándar:", value: "ARS" + formatoPrecio(total51(7199 ), "ARS"), inline: true },
-          { name: "Premium:", value: "ARS" + formatoPrecio(total51(9699 ), "ARS"), inline: true },
-          { name: "Casa extra:", value: "ARS" + formatoPrecio(total51(2499 ), "ARS"), inline: true }
+          { name: "Básico:", value: "ARS" + formatoPrecio(total51(5999), "ARS"), inline: true },
+          { name: "Estándar:", value: "ARS" + formatoPrecio(total51(9999), "ARS"), inline: true },
+          { name: "Premium:", value: "ARS" + formatoPrecio(total51(13499), "ARS"), inline: true },
+          { name: "Casa extra:", value: "ARS" + formatoPrecio(total51(3599), "ARS"), inline: true }
         )
 
 

d5ce242afb83f0eaf159586092b90575a6613aa3
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2025-02-25 09:51:07 -0300

    PASO Afuera

--- src/commands/elecciones.ts
+++ src/commands/elecciones.ts
@@ -15,7 +15,6 @@ module.exports = {
         .setDescription("Presidente y vicepresidente en el cargo:\n 🟣Javier Milei y Victoria Villaruel ")
         .setThumbnail("https://cdn.discordapp.com/attachments/802944543510495292/1177087620187181207/vote.png?ex=65713b45&is=655ec645&hm=9a58b3df161529243080c52f67b28a49c4b7433a130e431a1646b81bf173dacb&")
         .addFields(
-         { name: "Elecciones legislativas PASO 2025 (3 de agosto 2025)", value: "Faltan " + diasHasta(new Date("2025-08-03")) + " días para las siguientes elecciones legislativas "},
          { name: "Elecciones legislativas generales (26 de octubre 2025)", value: "Faltan " + diasHasta(new Date("2025-10-26")) + " días para las siguientes elecciones legislativas "},         
          )
       return interaction.reply({ embeds: [embed] });

1d7abcbf103354ed2ed78d582e2315e9a1866acb
GonzaAhrexd <gonzalonicolasebel@gmail.com>
2025-10-02 17:37:01 -0300

    Cambios Game Pass

--- src/commands/servicios/GamePass.ts
+++ src/commands/servicios/GamePass.ts
@@ -11,10 +11,9 @@ const gamepass = async (client: any, interaction: Discord.CommandInteraction) =>
         .setColor('#a6ed75')
         .setThumbnail("https://cdn.discordapp.com/attachments/802944543510495292/903853195443445770/xbox.png")
         .addFields(
-          { name: "Xbox Game Pass Core (Consola)", value: "ARS" + formatoPrecio((total21(5999.99)), "ARS"), inline: true },
-          { name: "Xbox Game Pass Standard (Consola)", value: "ARS" + formatoPrecio((total21(8999.99)), "ARS"), inline: true },
-          { name: "Xbox Game Pass para PC", value: "ARS" + formatoPrecio((total21(6999.00)), "ARS"), inline: true },
-          { name: "Xbox Game Pass Ultimate (PC y Consola)", value: "ARS" + formatoPrecio((total21(10999.00)), "ARS"), inline: true }
+          { name: "Xbox Game Pass Essential", value: "ARS" + formatoPrecio((total21(8999.99)), "ARS"), inline: true },
+          { name: "Xbox Game Pass Premium", value: "ARS" + formatoPrecio((total21(11999.99)), "ARS"), inline: true },
+          { name: "Xbox Game Pass Ultimate", value: "ARS" + formatoPrecio((total21(24999.00)), "ARS"), inline: true },
         )
         
       return interaction.reply({ embeds: [embed] });

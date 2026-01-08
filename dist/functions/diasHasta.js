3c79d2f8eeea85e622e925df95b062a1c4a9129f
Gonzalo Ebel <gonzalonicolasebel@gmail.com>
2026-01-08 16:30:13 -0300

    Uso de build optimizada en Heroku

--- dist/functions/diasHasta.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function diasHasta(fecha) {
-    let hoy = new Date();
-    //@ts-ignore
-    return Math.ceil((fecha - hoy) / (1000 * 60 * 60 * 24));
-}
-module.exports = {
-    diasHasta: diasHasta,
-};
--- dist/functions/numeroRandom.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function generarRandom(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-module.exports = {
-    generarRandom: generarRandom
-};
